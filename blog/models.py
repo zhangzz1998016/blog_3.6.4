c196d5601e47b89827718e69e7c06cf705d126c3
zhangzz19980116 <zhangzz19980116@126.com>
2020-03-05 09:55:33 +0800

    blog_3.6.4
    blog

---
+++ blog/models.py
@@ -0,0 +1,53 @@
+from django.db import models
+
+# Create your models here.
+from django.urls import reverse
+from django.utils import timezone
+from django.contrib.auth.models import User
+import markdown
+from django.utils.html import strip_tags
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=100)
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=70)
+    body = models.TextField()
+    created_time = models.DateTimeField()
+    modified_time = models.DateTimeField()
+    excerpt = models.CharField(max_length=200, blank=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    tags = models.ManyToManyField(Tag, blank=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+
+    class Meta:
+        verbose_name = '文章'
+        verbose_name_plural = verbose_name
+        ordering = ['-created_time']
+
+
+    def save(self, *args, **kwargs):
+        self.modified_time = timezone.now()
+        # 首先实例化一个 Markdown 类，用于渲染 body 的文本。
+        # 由于摘要并不需要生成文章目录，所以去掉了目录拓展。
+        md = markdown.Markdown(
+            extensions=["markdown.extensions.extra", "markdown.extensions.codehilite",]
+        )
+        # 先将 Markdown 文本渲染成 HTML 文本
+        # strip_tags 去掉 HTML 文本的全部 HTML 标签
+        # 从文本摘取前 54 个字符赋给 excerpt
+        self.excerpt = strip_tags(md.convert(self.body))[:54]
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.title
+        
+    def get_absolute_url(self):
+        return reverse('blog:detail', kwargs={'pk': self.pk})
---
+++ blog/views.py
@@ -0,0 +1,47 @@
+from django.shortcuts import render, get_object_or_404
+
+# Create your views here.
+
+from .models import Post
+import markdown
+from django.utils.text import slugify
+from markdown.extensions.toc import TocExtension
+import re
+from .models import Post, Category, Tag
+
+def index(request):
+    post_list = Post.objects.all()
+    return render(request, 'blog/index.html', context={'post_list': post_list})
+
+def detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    md = markdown.Markdown(extensions=[
+        'markdown.extensions.extra',
+        'markdown.extensions.codehilite',
+        # 记得在顶部引入 TocExtension 和 slugify
+        TocExtension(slugify=slugify),
+    ])
+    post.body = md.convert(post.body)
+    m = re.search(r'<div class="toc">\s*<ul>(.*)</ul>\s*</div>', md.toc, re.S)
+    post.toc = m.group(1) if m is not None else ''
+    return render(request, 'blog/detail.html', context={'post': post})
+
+def archive(request, year, month):
+    post_list = Post.objects.filter(created_time__year=year,
+                                    created_time__month=month
+                                    )
+    return render(request, 'blog/index.html', context={'post_list': post_list})
+
+
+def category(request, pk):
+    # 记得在开始部分导入 Category 类
+    cate = get_object_or_404(Category, pk=pk)
+    post_list = Post.objects.filter(category=cate)
+    return render(request, 'blog/index.html', context={'post_list': post_list})
+
+
+def tag(request, pk):
+    # 记得在开始部分导入 Tag 类
+    t = get_object_or_404(Tag, pk=pk)
+    post_list = Post.objects.filter(tags=t)
+    return render(request, 'blog/index.html', context={'post_list': post_list})
