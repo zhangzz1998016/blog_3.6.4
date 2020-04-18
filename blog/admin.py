348f0e678a5877acec1f9aab18ffa09c232beee5
zhangzz1998016 <61784992+zhangzz1998016@users.noreply.github.com>
2020-04-18 23:02:36 +0800

    尝试后台第一次

--- blog/admin.py
+++ blog/admin.py
@@ -3,7 +3,10 @@ from django.contrib import admin
 # Register your models here.
 from django.contrib import admin
 from .models import Post, Category, Tag
- 
-admin.site.register(Post)
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['title', 'created_time', 'modified_time', 'category', 'author']
+
+admin.site.register(Post, PostAdmin)
 admin.site.register(Category)
 admin.site.register(Tag)
--- blog/models.py
+++ blog/models.py
@@ -11,20 +11,33 @@ from django.utils.html import strip_tags
 class Category(models.Model):
     name = models.CharField(max_length=100)
 
+    class Meta:
+        verbose_name = '分类'
+        verbose_name_plural = verbose_name
+
+    def __str__(self):
+        return self.name
 
 class Tag(models.Model):
     name = models.CharField(max_length=100)
 
+    class Meta:
+        verbose_name = '标签'
+        verbose_name_plural = verbose_name
+
+    def __str__(self):
+        return self.name
+
 
 class Post(models.Model):
-    title = models.CharField(max_length=70)
-    body = models.TextField()
-    created_time = models.DateTimeField()
-    modified_time = models.DateTimeField()
+    title = models.CharField('标题', max_length=70)
+    body = models.TextField('正文')
+    created_time = models.DateTimeField('创建时间')
+    modified_time = models.DateTimeField('修改时间')
     excerpt = models.CharField(max_length=200, blank=True)
-    category = models.ForeignKey(Category, on_delete=models.CASCADE)
-    tags = models.ManyToManyField(Tag, blank=True)
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, verbose_name='分类', on_delete=models.CASCADE)
+    tags = models.ManyToManyField(Tag, verbose_name='标签', blank=True)
+    author = models.ForeignKey(User, verbose_name='作者', on_delete=models.CASCADE)
 
 
     class Meta:
@@ -48,6 +61,6 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
-        
+
     def get_absolute_url(self):
         return reverse('blog:detail', kwargs={'pk': self.pk})
